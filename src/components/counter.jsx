3158b508e0a0876623b48593b0cb92d9720a98e7
Dillon <dillon.embreus@gmail.com>
2018-09-13 18:14:50 -0600

    started tutorial with mosh

---
+++ src/components/counter.jsx
@@ -0,0 +1,49 @@
+import React, { Component, Fragment } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
+
+  //right here time {107.11}
+  handleIncrement = () => {
+    console.log("Increment clicked", this.state.count);
+  };
+  render() {
+    return (
+      <Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement()}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
