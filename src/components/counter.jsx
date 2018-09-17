e14b598eeb85ec1638606d7d073e3b6e4cf88b12
Dillon <dillon.embreus@gmail.com>
2018-09-16 23:58:27 -0600

    Added component
    created counters component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,47 +2,37 @@ import React, { Component, Fragment } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.value
   };
 
-  //   constructor() {
-  //     super();
-  //     this.handleIncrement = this.handleIncrement.bind(this);
-  //   }
-
-  //right here time {107.11}
   handleIncrement = () => {
-    console.log("Increment clicked", this.state.count);
+    this.setState({ value: this.state.value + 1 });
   };
+
   render() {
+    console.log("props", this.props);
     return (
       <Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement()}
+          onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
       </Fragment>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
